d3dc952e537df47a0dcdb258dc7258a7a08e533c
nathan <ntwilson@gmail.com>
2019-10-01 22:32:17 -0400

    code cleanup.  remove the unsafePartial

--- src/Role.Harvester.purs
+++ src/Role.Harvester.purs
@@ -2,8 +2,8 @@ module Role.Harvester (run) where
 
 import Prelude
 
-import Data.Array.Partial (head)
-import Data.Maybe (fromJust)
+import Data.Array (head)
+import Data.Maybe (Maybe(..), fromJust)
 import Effect (Effect)
 import Foreign.Object (lookup)
 import Partial.Unsafe (unsafePartial)
@@ -14,22 +14,31 @@ import Screeps.Room (find)
 import Screeps.RoomObject (room)
 import Screeps.Types (Creep, ResourceType(..), TargetPosition(..))
 
+ignore :: forall a. a -> Unit
+ignore _ = unit
+
+ignoreF :: forall f a. Functor f => f a -> f Unit
+ignoreF f = f <#> ignore 
+
 run :: Creep -> Effect Unit
-run creep = unsafePartial do
+run creep = do
   game <- getGameGlobal
 
   if amtCarrying creep (ResourceType "energy") < carryCapacity creep
-  then do
-    let sources = find (room creep) find_sources
-    let targetSource = head sources
-    harvestResult <- harvestSource creep targetSource
-    if harvestResult == err_not_in_range
-    then moveTo creep (TargetObj targetSource) <#> (\_ -> unit)
-    else pure unit
+  then
+    case head (find (room creep) find_sources) of
+      Nothing -> pure unit
+      Just targetSource -> do
+        harvestResult <- harvestSource creep targetSource
+        if harvestResult == err_not_in_range
+        then moveTo creep (TargetObj targetSource) # ignoreF
+        else pure unit
 
-  else do
-    let spawn1 = (spawns game # lookup "Spawn1" # fromJust)
-    transferResult <- transferToStructure creep spawn1 resource_energy
-    if transferResult == err_not_in_range
-    then moveTo creep (TargetObj spawn1) <#> (\_ -> unit)
-    else pure unit
+  else
+    case (spawns game # lookup "Spawn1") of
+      Nothing -> pure unit
+      Just spawn1 -> do
+        transferResult <- transferToStructure creep spawn1 resource_energy
+        if transferResult == err_not_in_range
+        then moveTo creep (TargetObj spawn1) # ignoreF
+        else pure unit
