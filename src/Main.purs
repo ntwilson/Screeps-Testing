c35bef7567125d5ef312c1d7d8bd14b667f11e4e
Alex Berkhout <berkhoutalex@gmail.com>
2019-10-02 18:48:59 -0500

    working on the 3rd tutorial

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,44 @@
-module Main where
+module Main (loop) where
 
 import Prelude
 
+import Data.Foldable (for_)
 import Effect (Effect)
-import Effect.Console (log)
+import Data.Maybe (Maybe(..))
+import Role.Harvester (runHarvester)
+import Role.Upgrader (runUpgrader)
+import Screeps.Types (Creep)
+import Screeps.Game (creeps, getGameGlobal)
+import Screeps.Creep (getMemory)
+import Data.Either (either, fromRight, isRight)
+import Partial
 
-main :: Effect Unit
-main = do
-  log "🍝"
+ignore :: forall a. a -> Unit
+ignore _ = unit
+
+ignoreM :: forall m a. Monad m => m a -> m Unit
+ignoreM m = m <#> ignore 
+
+returnUnit :: forall a. a -> Effect Unit
+returnUnit a = pure unit
+
+matchUnit :: Creep -> String -> Effect Unit
+matchUnit creep role = 
+  if role == "harvester"
+    then
+      (runHarvester creep)
+    else 
+      (runUpgrader creep)
+
+runCreepRole :: Creep -> Effect Unit
+runCreepRole creep = 
+  do
+    role <- getMemory creep "role"
+    either returnUnit (matchUnit creep) role
+
+loop :: Effect Unit
+loop = do
+  game <- getGameGlobal
+  for_ (creeps game) runCreepRole
+    
+
---
+++ src/Role.Harvester.purs
@@ -0,0 +1,43 @@
+module Role.Harvester (runHarvester) where
+
+import Prelude
+
+import Data.Array (head)
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Foreign.Object (lookup)
+import Screeps (err_not_in_range, find_sources, resource_energy)
+import Screeps.Creep (amtCarrying, carryCapacity, harvestSource, moveTo, transferToStructure)
+import Screeps.Game (getGameGlobal, spawns)
+import Screeps.Room (find)
+import Screeps.RoomObject (room)
+import Screeps.Types (Creep, ResourceType(..), TargetPosition(..))
+
+ignore :: forall a. a -> Unit
+ignore _ = unit
+
+ignoreM :: forall m a. Monad m => m a -> m Unit
+ignoreM m = m <#> ignore 
+
+runHarvester :: Creep -> Effect Unit
+runHarvester creep =
+
+  if amtCarrying creep (ResourceType "energy") < carryCapacity creep
+  then
+    case head (find (room creep) find_sources) of
+      Nothing -> pure unit
+      Just targetSource -> do
+        harvestResult <- harvestSource creep targetSource
+        if harvestResult == err_not_in_range
+        then moveTo creep (TargetObj targetSource) # ignoreM
+        else pure unit
+        
+  else do
+    game <- getGameGlobal
+    case (spawns game # lookup "Spawn1") of
+      Nothing -> pure unit
+      Just spawn1 -> do
+        transferResult <- transferToStructure creep spawn1 resource_energy
+        if transferResult == err_not_in_range
+        then moveTo creep (TargetObj spawn1) # ignoreM
+        else pure unit
