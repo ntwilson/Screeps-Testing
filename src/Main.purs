38827242ee88940cdf34c8c3ef3b62cc823ab61c
Alex Berkhout <berkhoutalex@gmail.com>
2019-10-05 19:44:53 -0500

    work in progress automatic spawning. Need to figure out how to correctly set the role from code

--- src/Main.purs
+++ src/Main.purs
@@ -2,18 +2,28 @@ module Main (loop) where
 
 import Prelude
 
+import Data.Array (head)
 import Data.Either (Either(..))
 import Data.Foldable (for_)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Class.Console (error)
+import Effect.Class.Console (error, logShow)
 import Effect.Console (log)
+import Effect.Unsafe (unsafePerformEffect)
+import Foreign.Object (filter, isEmpty, size)
+import Math (e)
+import Role.Builder (runBuilder)
 import Role.Harvester (runHarvester)
 import Role.Upgrader (runUpgrader)
-import Screeps.Creep (getMemory)
-import Screeps.Game (creeps, getGameGlobal)
-import Screeps.Types (Creep)
-import Role.Builder (runBuilder)
+import Screeps (find_my_spawns)
+import Screeps.Constants (part_carry, part_move, part_work)
+import Screeps.Creep (getMemory, setMemory)
+import Screeps.Game (creeps, getGameGlobal, spawns)
+import Screeps.Memory (toJson)
+import Screeps.Room (find)
+import Screeps.RoomObject (room)
+import Screeps.Spawn (canCreateCreep, createCreep')
+import Screeps.Types (Creep, RawCreep, RawRoomObject)
 
 ignore :: forall a. a -> Unit
 ignore _ = unit
@@ -36,27 +46,71 @@ matchUnit creep role = do
 runCreepRole :: Creep -> Effect Unit
 runCreepRole creep = 
   do
-    role <- getMemory creep "role"
+    role <- getMemory creep "\"role\""
     case role of
       Right json -> 
         matchUnit creep json
       Left e ->
         log e
 
--- spawnNewCreeps :: Unit -> Effect Unit
--- spawnNewCreeps =
---   let 
---     minHarvesters :: int
---     minHarvesters = 5
---     minBuilder :: int
---     minBuilders = 2
---   in
-    
+creepToRole :: (RawRoomObject RawCreep) -> Effect String
+creepToRole creep = do
+  role <- (getMemory creep "\"role\"")
+  case role of
+    Left e -> do
+      pure "undefined"
+    Right role ->
+      pure role
+
+creepIsRole :: (RawRoomObject RawCreep) -> String -> Effect Boolean
+creepIsRole creep role = do
+  creepRole <- creepToRole creep
+  pure (role == creepRole)
+
+spawnNewCreeps :: Creep -> Effect Unit
+spawnNewCreeps creep =
+ let 
+   minHarvesters :: Int
+   minHarvesters = 5
+   minBuilder :: Int
+   minBuilder = 2
+ in 
+    do
+      thisGame <- getGameGlobal
+      harvesters <- do (pure (filter (\n -> (unsafePerformEffect (creepIsRole n "Harvester"))) (creeps thisGame))) 
+      upgraders <- do (pure (filter (\n -> (unsafePerformEffect (creepIsRole n "Upgrader"))) (creeps thisGame))) 
+      builders <- do (pure (filter (\n -> (unsafePerformEffect (creepIsRole n "Builder"))) (creeps thisGame))) 
+      case (head (find (room creep) find_my_spawns)) of
+        Nothing -> pure unit
+        Just spawn -> 
+          if ((size harvesters) < minHarvesters) then do
+            r <- createCreep' spawn [part_move, part_work, part_work, part_carry] Nothing "{\"role\":\"Harvester\"}"
+            case r of
+              Right e -> logShow e
+              Left code -> logShow code
+          else
+            if ((size builders) < minBuilder) then do
+              r <- createCreep' spawn [part_move, part_move, part_work, part_carry] Nothing "{\"role\":\"Builder\", \"Building\":\"false\"}"
+              case r of
+                Right e -> logShow e
+                Left code -> logShow code
+            else do
+              r <- createCreep' spawn [part_move, part_work, part_work, part_carry] Nothing "{{\"memory\": \"role\":\"Upgrader\"}}"
+              case r of
+                Right e -> logShow e
+                Left code -> logShow code
 
 
 loop :: Effect Unit
 loop = do
   game <- getGameGlobal
-  for_ (creeps game) runCreepRole
+  if (isEmpty (creeps game)) then
+    for_ (spawns game) \spawn -> do
+      createCreep' spawn [part_move, part_move, part_work, part_carry] Nothing "{{\"memory\": \"role\":\"Upgrader\"}}"
+  else  
+    pure unit
+  for_ (creeps game) \n -> do
+    spawnNewCreeps n
+    runCreepRole n
     
 
